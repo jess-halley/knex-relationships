90d3988d394d19e8f43f20fbc914cfbfc81adb42
mischa-saunders <mischa.saunders@gmail.com>
2017-05-10 16:31:36 +1200

    forms port live

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,24 @@ var router = express.Router()
 
 var db = require('../db')
 
+router.get('/profile/new', (req, res) => {
+  res.render('form')
+})
+
+
+router.get('/profile/:id', function (req, res){
+  var connection = req.app.get('connection')
+  var id = req.params.id
+  db.getUser(id, connection)
+  .then((results) => {
+    // console.log({results});
+    res.render('profile.hbs', results)
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
+})
+
 router.get('/', function (req, res) {
   db.getUsers(req.app.get('connection'))
     .then(function (users) {
@@ -14,17 +32,16 @@ router.get('/', function (req, res) {
 })
 
 
-router.get('/profile/:id', function (req, res){
-  var connection = req.app.get('connection')
-  var id = req.params.id
-  db.getUser(id, connection)
-  .then((results) => {
-    console.log({results});
-    res.render('profile.hbs', results)
-  })
-  .catch(function (err) {
-    res.status(500).send('DATABASE ERROR: ' + err.message)
-  })
-})
+
+// router.get('/newUser', function (req, res){
+//   db.getUsers(req.app.get('connection'))
+//   res.send('forms.hbs')
+// })
+// .catch(function (err) {
+//   res.status(500).send('DATABASE ERROR: ' + err.message)
+// })
+
+
+
 
 module.exports = router
